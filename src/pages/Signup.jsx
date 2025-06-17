a8e032060c8ea4e728cbbb353ab0c18409a47fd1
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-06-17 22:22:07 +0530

    signup page starting

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Template from '../components/core/Auth/Template.jsx'
+
+function Signup() {
+  return (
+    <div>
+        <Template
+        title="Create an account"
+        formType="signup"
+        />
+    </div>
+  )
+}
+
+export default Signup
