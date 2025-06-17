88e4215e2baa9b6f249dd2d2f08965c9f4db82df
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-06-17 22:24:06 +0530

    modifying app index main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import OpenRoute from './components/core/Auth/OpenRoute.jsx';
+import Signup from './pages/Signup.jsx';
 
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/" element={<h1>Welcome!</h1>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './reducer/index.js';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.jsx';
+import { configureStore } from '@reduxjs/toolkit';
+import { Toaster } from 'react-hot-toast';
+
+const store = configureStore({
+	reducer: rootReducer,
+});
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+				<Toaster></Toaster>
+			</BrowserRouter>
+		</Provider>
 	</StrictMode>,
 );
