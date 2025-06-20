e5619e30d8ae117afb44675cf11bbcd1a1e824c4
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-06-20 21:00:32 +0530

    adding the endpoints

---
+++ src/services/apis.js
@@ -0,0 +1,38 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+// auth endpoints
+export const authEndpoints = {
+	SENDOTP_API: BASE_URL + '/auth/sendOtp',
+	SIGNUP_API: BASE_URL + '/auth/register',
+	LOGIN_API: BASE_URL + '/auth/login',
+	//profile api ?
+};
+
+// goal endpoints
+export const goalEndpoints = {
+	CREATE_GOAL_API: BASE_URL + '/goals/create',
+	GET_USER_GOALS_API: BASE_URL + '/goals/',
+	UPDATE_GOAL_API: (goalId) => BASE_URL + `/goals/${goalId}`,
+	DELETE_GOAL_API: (goalId) => BASE_URL + `/goals/${goalId}`,
+};
+
+// pomodoro endpoints
+export const pomodoroEndpoints = {
+	// pending
+};
+
+// summary endpoints
+export const summaryEndpoints = {
+	GET_TODAY_SUMMARY_API: BASE_URL + '/summary/',
+	UPDATE_SUMMARY_API: BASE_URL + '/summary/update',
+};
+
+// task endpoints
+export const taskEndpoints = {
+	CREATE_TASK_API: BASE_URL + '/tasks/create',
+	GET_ALL_TASK_API: BASE_URL + '/tasks/my-tasks',
+	UPDATE_TASK_API: (taskId) => BASE_URL + `/tasks/update/${taskId}`,
+	DELETE_TASK_API: (taskId) => BASE_URL + `/tasks/delete/${taskId}`,
+	GET_KANBAN_API: BASE_URL + '/tasks/kanban',
+	UPDATE_TASK_STATUS_API: (taskId) => BASE_URL + `/tasks/${taskId}/status`,
+};
