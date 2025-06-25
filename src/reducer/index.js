8a12406e156d72b3e19d77ee17a72bd179ccafbc
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-06-25 14:41:00 +0530

    .store

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import authReducer from '../slices/authSlice';
+import goalReducer from '../slices/goalSlice.js';
+import pomodoroReducer from '../slices/pomodoroSlice.js';
+import dailySummaryReducer from '../slices/dailySummarySlice';
+import profileReducer from '../slices/profileSlice.js';
 
 const rootReducer = combineReducers({
 	auth: authReducer,
+	goal: goalReducer,
+	pomodoro: pomodoroReducer,
+	dailySummary: dailySummaryReducer,
+	profile: profileReducer,
 	// more to come
 });
 
